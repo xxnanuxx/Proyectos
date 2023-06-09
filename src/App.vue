ba25f21893fbf3ad9629c03a2c47548612c8bd86
xXNanuXx <nanu1429@gmail.com>
2023-06-08 21:08:19 -0300

    Inicio

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="navbar-brand">
+                <img src="/src/assets/logo.svg" width="30" height="30">
+              </a>
+            </li>
+             <li class="nav-item" style="margin-left:50px">
+              <input class="form-control mr-sm-2" type="search" placeholder="Ingrese el libro">
+            </li>
+             <li class="nav-item" style="margin-left:5px">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            </li>
+             <li class="nav-item" style="margin-left:50px">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Iniciar Sesion</button>
+            </li>
+          </ul>
+        </div>
+    </nav>
+  <RouterView />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script >
+</script>
+
+<template>
+  <main>
+    Home
+  </main>
+</template>
