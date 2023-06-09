18d00c5038b2553bbdfbe937dd1d319595173b1d
xXNanuXx <nanu1429@gmail.com>
2023-06-08 23:03:27 -0300

    Nuevas pantallas

---
+++ src/views/InicioSesionView.vue
@@ -0,0 +1,40 @@
+
+<template>
+<div class="container">
+    <div class="col" style="margin-top:20px">
+        <div class="row">
+            <h4>INICIO DE SESION</h4>
+        </div>
+        <div class="row" style="width:50%">
+            <input placeholder="Correo Electronico" type="search" class="form-control">
+        </div>
+        <div class="row" style="margin-top:10px; width:50%">
+            <input placeholder="Constraseña" type="password" class="form-control">
+        </div>
+        <div class="row" style="margin-top:15px; width:50%">
+            <p class="texto">¿No tiene cuenta creada?</p>
+        </div>
+        <div class="row" style="margin-top:-10px; width:50%">
+            <button class="btn btn-outline-success" @click="redirigir" type="submit">CREAR CUENTA</button>
+        </div>
+    </div>
+</div>
+</template>
+
+
+<style>
+.texto {
+  font-size:13px;
+  color: darkgrey;
+}
+</style>
+
+<script>
+export default {
+    methods:{
+        redirigir(){
+            this.$router.push("/Registrar")
+        }
+    }
+}
+</script>
---
+++ src/views/RegistrarView.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container">
+    <div class="col" style="margin-top:20px">
+        <div class="row">
+            <h4>REGISTRAR USUARIO</h4>
+        </div>
+        <div class="row" style="width:50%">
+            <input placeholder="Nombre" type="search" class="form-control">
+        </div>
+        <div class="row" style="margin-top:10px; width:50%">
+            <input placeholder="Apellido" type="search" class="form-control">
+        </div>
+        <div class="row" style="margin-top:10px; width:50%">
+            <input placeholder="Correo Electronico" type="search" class="form-control">
+        </div>
+        <div class="row" style="margin-top:10px; width:50%">
+            <input placeholder="Contraseña" type="search" class="form-control">
+        </div>
+    </div>
+</div>
+</template>
