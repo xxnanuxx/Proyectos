a22d8116694d590b8fc593bb7e45a93a0cc2cd7d
xXNanuXx <nanu1429@gmail.com>
2023-06-21 15:42:11 -0300

    Correcciones del jefe

--- src/views/InicioSesionView.vue
+++ src/views/InicioSesionView.vue
@@ -10,10 +10,10 @@
             <input v-model="usuario.email" placeholder="Correo Electronico" type="email" class="form-control">
         </div>
         <div class="row" style="margin-top:10px; width:50%">
-            <input v-model="usuario.contraseña" placeholder="Constraseña" type="password" class="form-control">
+            <input v-model="usuario.passw" placeholder="Constraseña" type="password" class="form-control">
         </div>
         <div class="row" style="margin-top:10px; width:50%">
-            <button class="btn btn-outline-success" @click="login" type="submit">INICIAR SESION</button>
+            <button class="btn btn-outline-success" @click="logear" type="submit">INICIAR SESION</button>
         </div>
         <div class="row" style="margin-top:15px; width:50%">
             <p class="texto">¿No tiene cuenta creada?</p>
@@ -45,23 +45,23 @@ export default {
         const store = useLoginStore();
         const { login } = store;
         return { login };
-    }
-
+    },
+    methods: { 
+        logear() {
+            if(this.usuario.email=="usuario@test.com" && this.usuario.passw=="123456") {
+                this.login({email:this.usuario.email, permissions:[]})
+                this.$router.push("/")
+            } else if(this.usuario.email=="admin@test.com" && this.usuario.passw=="123456") {
+                this.login({email:this.usuario.email,permissions: ['BooksTable']})
+                this.$router.push("/")
+            } else {
+                alert('Credenciales erroneas')
+            };
+        },
+        redirigir(){
+            this.$router.push("/Registrar")
+        }
+     }
 }
-
-   
-    methods: {
-    logear() 
-    { if(this.usuario.email=="usuario@test.com" && this.usuario.passw=="123456") {
-        this.login({email:this.usuario.email, permissions:[]})
-        this.$router.push("/")
-      } else if(this.usuario.email=="admin@test.com" && this.usuario.passw=="123456") {
-        this.login({email:this.usuario.email,permissions: ['BooksTable']})
-        this.$router.push("/")
-      } else {
-        alert('Credenciales erroneas')
-      };
-    }
-  };
 
 </script>
